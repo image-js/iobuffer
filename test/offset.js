31047487bcc70ee8345afb64173dbb0f0e1f00b9
Daniel Kostro <kostro.d@gmail.com>
2016-12-13 10:20:34 +0100

    tests: remove .only

--- test/offset.js
+++ test/offset.js
@@ -3,7 +3,7 @@
 const IOBuffer = require('..');
 
 describe('test with offset', function () {
-    it.only('should accept other IOBuffer with offset option', function () {
+    it('should accept other IOBuffer with offset option', function () {
         const io1 = new IOBuffer(10);
         io1.writeBytes([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
         const io2 = new IOBuffer(io1, {offset: 4});
