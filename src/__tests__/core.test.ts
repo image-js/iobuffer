d5c070c398cf9bc8031afdb369d6d3a4071bdef5
Michaël Zasso <targos@protonmail.com>
2025-07-17 11:24:17 +0200

    chore: update dev dependencies

--- src/__tests__/core.test.ts
+++ src/__tests__/core.test.ts
@@ -4,6 +4,7 @@ import { IOBuffer } from '../iobuffer.ts';
 
 describe('core methods', () => {
   let buffer: IOBuffer;
+
   beforeEach(() => {
     buffer = new IOBuffer();
   });
@@ -14,13 +15,18 @@ describe('core methods', () => {
 
   it('should report availability', () => {
     buffer.length = 15;
+
     expect(buffer.available(15)).toBe(true);
     expect(buffer.available(20)).toBe(false);
+
     buffer.skip(1);
+
     expect(buffer.available(15)).toBe(false);
     expect(buffer.available(14)).toBe(true);
     expect(buffer.available(5)).toBe(true);
+
     buffer.seek(14);
+
     expect(buffer.available()).toBe(true);
     expect(buffer.available(1)).toBe(true);
     expect(buffer.available(2)).toBe(false);
@@ -29,39 +35,56 @@ describe('core methods', () => {
   it('get/set endianess', () => {
     expect(buffer.isLittleEndian()).toBe(true);
     expect(buffer.isBigEndian()).toBe(false);
+
     buffer.setBigEndian();
+
     expect(buffer.isLittleEndian()).toBe(false);
     expect(buffer.isBigEndian()).toBe(true);
+
     buffer.setLittleEndian();
+
     expect(buffer.isLittleEndian()).toBe(true);
     expect(buffer.isBigEndian()).toBe(false);
   });
 
   it('skip', () => {
     buffer.skip();
+
     expect(buffer.offset).toBe(1);
+
     buffer.skip();
     buffer.skip(1);
+
     expect(buffer.offset).toBe(3);
+
     buffer.skip(5);
+
     expect(buffer.offset).toBe(8);
   });
 
   it('back', () => {
     buffer.offset = 8;
     buffer.back(5);
+
     expect(buffer.offset).toBe(3);
+
     buffer.back(1);
     buffer.back();
+
     expect(buffer.offset).toBe(1);
+
     buffer.back();
+
     expect(buffer.offset).toBe(0);
   });
 
   it('seek', () => {
     buffer.seek(0);
+
     expect(buffer.offset).toBe(0);
+
     buffer.seek(12);
+
     expect(buffer.offset).toBe(12);
   });
 
@@ -71,6 +94,7 @@ describe('core methods', () => {
     buffer.skip(2);
     buffer.seek(3);
     buffer.reset();
+
     expect(buffer.offset).toBe(12);
   });
 
@@ -79,15 +103,22 @@ describe('core methods', () => {
     buffer.pushMark();
     buffer.seek(10);
     buffer.popMark();
+
     expect(buffer.offset).toBe(5);
+
     buffer.pushMark();
     buffer.seek(12);
     buffer.pushMark();
     buffer.skip(1);
+
     expect(buffer.offset).toBe(13);
+
     buffer.popMark();
+
     expect(buffer.offset).toBe(12);
+
     buffer.popMark();
+
     expect(buffer.offset).toBe(5);
     expect(() => {
       buffer.popMark();
@@ -97,11 +128,13 @@ describe('core methods', () => {
   it('rewind', () => {
     buffer.seek(10);
     buffer.rewind();
+
     expect(buffer.offset).toBe(0);
   });
 
   it('is chainable', () => {
     const io = new IOBuffer();
+
     expect(() => {
       io.writeChars('abc')
         .writeUint32(10)
